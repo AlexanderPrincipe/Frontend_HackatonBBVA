f55f06fff489b532f484850147e156d5326e0ff2
miguel.ugarte.contractor@bbva.com <miguel.ugarte.contractor@bbva.com>
2022-08-28 15:38:53 -0500

    add: added new modal with properties and maps

--- src/app/components/home-aforo/home-aforo.component.ts
+++ src/app/components/home-aforo/home-aforo.component.ts
@@ -9,18 +9,38 @@ export class HomeAforoComponent implements OnInit {
 
   @Input() arrDistanceNear: any = [];
   @Input() recomendado: any = {};
-  modalTitle = "";
-  oficina = "";
-  aforo = "";
+  coors: any;
+  address: any;
+  office: any;
+  cantCaj: any;
+  cantOut: any;
+  cantVen: any;
+  cantPla: any;
+  timeAte: any;
+  name: any;
 
   constructor() { }
 
   ngOnInit(): void {
   }
-    addClick() {
-    this.modalTitle = "Vista de Oficina";
-    this.oficina = "";
-    this.aforo = "";
+
+  open(event: any, item: any){
+    console.log(item)
+    this.coors = `https://maps.google.com/maps?q=${item.lat},${item.long}&hl=es;z=14&amp;output=embed`;
+    const maps = document.querySelector('#map');
+    if(maps){
+      console.log("🚀 ~ file: home-aforo.component.ts ~ line 33 ~ HomeAforoComponent ~ open ~ maps", maps);
+      maps.setAttribute('src',this.coors);
+    }
+    this.address = item.address;
+    this.office = (item.process.cantidad / item.aforoMax) * 100;
+    this.timeAte = `${Math.floor((Math.random()*10)+1)} min`;
+    this.name = item.name;
+    this.cantCaj = Math.floor((Math.random()*10)+1);
+    this.cantOut = Math.floor((Math.random()*10)+1);
+    this.cantVen = Math.floor((Math.random()*10)+1);
+    this.cantPla = Math.floor((Math.random()*10)+1);
+    
   }
 }
 
